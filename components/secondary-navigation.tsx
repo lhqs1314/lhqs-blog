2b671a676701db6c1e0ca229dbe82698b9110f33
lhqs <lhqs1314@gmail.com>
2025-07-09 10:16:24 +0800

    fix: update content

--- components/secondary-navigation.tsx
+++ components/secondary-navigation.tsx
@@ -71,7 +71,7 @@ export default function SecondaryNavigation() {
   if (pathname === '/') {
     sectionTitle = '关于';
   } else if (pathname.startsWith('/thoughts')) {
-    sectionTitle = '文章';
+    sectionTitle = '碎碎念';
   } else if (pathname.startsWith('/projects')) {
     sectionTitle = '项目';
   } else if (pathname.startsWith('/guestbook')) {
